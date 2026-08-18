073e7cb9f478df68e58c8acdcc4ae6695f17dc0b
ACCDevHSA <MANPOWERGabriel_Santana@honda.com.br>
2026-08-18 16:06:48 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: 'https://xxxxxxxx.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJ.....'
+  SUPABASE_URL: 'https://slbwszjlrpwrgkgtsfnk.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_ljR8FTbX6eB_P_VeLck28A_By8nIq34'
 };
 ``
