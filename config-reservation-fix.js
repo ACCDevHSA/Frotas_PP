296cb6ffee2fe2385772ad7fa411cbc324c253cb
ACCDevHSA <MANPOWERGabriel_Santana@honda.com.br>
2026-09-16 13:50:31 -0300

    Add files via upload

---
+++ config-reservation-fix.js
@@ -0,0 +1,3 @@
+/* Cole ao final do config.js e preencha com os valores EXATOS que seu projeto já usa. */
+window.FLEET_RESERVATIONS_TABLE = window.FLEET_RESERVATIONS_TABLE || "";
+window.FLEET_CANCELLED_STATUS = window.FLEET_CANCELLED_STATUS || "cancelled";
